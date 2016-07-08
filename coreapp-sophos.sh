47b423afd7c42fb70de43422bc6390d746e47e69
Geoff Lee <g.lee@ed.ac.uk>
2016-07-08 12:02:06 +0100

    Import slightly modified script from dave Savage's package

--- coreapp-sophos.sh
+++ coreapp-sophos.sh
@@ -1,7 +1,52 @@
 #!/bin/sh
 
-# Just a test for the moment, to see what if we are being 
-# passed parameters in the way I think we are
+## Script to install Sophos Antivirus
 
-echo $@
+$TEMP_DIR="/Library/Application Support/JAMF/tmp/sophos"
+$UPDATE_SERVER="$4"
+$INSTALL_FILE="$5"
+
+# Create temporary work area if it doesn't exist
+[ ! -d "${TEMP_DIR}" ] && mkdir -p "${TEMP_DIR}"
+
+# Check if Sophos has been previously installed
+if [ -f "/Library/Caches/com.sophos.sau/CID/cidsync.upd" ]
+then
+        version=`defaults read /Applications/Sophos\ Anti-Virus.app/Contents/Info CFBundleShortVersionString | awk -F "." '{print $1}'`
+        if [ $version == 9 ]
+        then
+                # Disable web protection - it leaks information and slows down web browsing
+                defaults write /Library/Preferences/com.sophos.sav WebProtectionFilteringEnabled -bool false
+                defaults write /Library/Preferences/com.sophos.sav WebProtectionScanningEnabled -bool false
+        
+                exit 0;
+        else
+                # Scrub the autoupdate cache and lockfile then do an update (pkg installs new update server)
+                rm -f /Library/Caches/com.sophos.sau/CID/cidsync.upd
+                rm -f /Library/Caches/com.sophos.sau/sophosautoupdate.plist
+                rm -f /Library/Preferences/com.sophos.sau.plist.lockfile                
+                sleep 1
+                rm -dfR /Library/Caches/com.sophos.sau
+        fi      
+fi
+
+# Download and install Sophos AV
+
+# Rather than try to keep up with Sophos upgrades, download the newest version from the local source
+/usr/bin/curl ${UPDATE_SERVER}/${INSTALL_FILE} > ${TEMP_DIR}/
+cd "${TEMP_DIR}"
+unzip ${INSTALL_FILE} 
+
+# Install Sophos
+
+"${TEMP_DIR}/Sophos Installer.app/Contents/MacOS/tools/InstallationDeployer" --install
+
+if [ "$?" == 0 ]
+then
+    logger "$0: Installed Sophos"
+    exit 0
+else
+    logger "$0: Failed to install Sophos. Error Code: ${?}"
+    exit 255
+fi
 
