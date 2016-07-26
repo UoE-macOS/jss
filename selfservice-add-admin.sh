e2b5e7ee1fca69355c26f52acbadf24ef6809c3e
Geoff Lee <g.lee@ed.ac.uk>
2016-07-26 10:02:44 +0100

    Various tweaks to the launchd commands and revoke script. Use a heredoc to minimise quoting agony

--- selfservice-add-admin.sh
+++ selfservice-add-admin.sh
@@ -2,15 +2,16 @@
 
 ##############
 # TempAdmin.sh
-# This script will give a user 30 minutes of Admin level access.
+# This script will give a user 15 minutes of Admin level access.
 # It is designed to create its own offline self-destruct mechanism.
 ##############
 
 USERNAME=`who |grep console| awk '{print $1}'`
+LOGS='/var/admin-logs'
 
 # create LaunchDaemon to remove admin rights
 #####
-echo "<?xml version="1.0" encoding="UTF-8"?> 
+echo '<?xml version="1.0" encoding="UTF-8"?> 
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"> 
 <plist version="1.0"> 
 <dict>
@@ -25,20 +26,30 @@ echo "<?xml version="1.0" encoding="UTF-8"?>
     <key>StartInterval</key>
     <integer>900</integer> 
 </dict> 
-</plist>" > /Library/LaunchDaemons/uk.ac.ed.adminremove.plist
+</plist>' > /Library/LaunchDaemons/uk.ac.ed.adminremove.plist
 #####
 
 # create admin rights removal script
 #####
-echo '#!/bin/bash
-osascript -e '"'"'display notification "Local administrator privileges have been revoked" with title "Admin Revoked"'"'"'
-USERNAME=`cat /var/admin-logs/userToRemove`
+
+cat > /Library/Scripts/removeTempAdmin.sh << EOT
+#!/bin/bash
+USERNAME="\$(cat ${LOGS}/userToRemove)"
+TIME="\$(date '+Date:%m-%d-%Y TIME:%H:%M:%S')"
+
 /usr/sbin/dseditgroup -o edit -d $USERNAME -t user admin
-rm -f /var/admin-logs/userToRemove
+echo "\$TIME REVOKED \$USERNAME" >> "${LOGS}/15minAdmin"
+
+rm -f "${LOGS}/userToRemove"
+
+launchctl unload -w  /Library/LaunchDaemons/uk.ac.ed.adminremove.plist
 rm -f /Library/LaunchDaemons/uk.ac.ed.adminremove.plist
 rm -f /Library/Scripts/removeTempAdmin.sh
-exit 0' > /Library/Scripts/removeTempAdmin.sh  
-#####
+
+## What happens if nobody is logged in?
+osascript -e 'display notification "Local administrator privileges have been revoked" with title "Admin Revoked"'
+exit 0
+EOT
 
 # set the permission on the files just made
 chown root:wheel /Library/LaunchDaemons/uk.ac.ed.adminremove.plist
@@ -47,16 +58,15 @@ chown root:wheel /Library/Scripts/removeTempAdmin.sh
 chmod 755 /Library/Scripts/removeTempAdmin.sh
 
 # enable and load the LaunchDaemon
-defaults write /Library/LaunchDaemons/uk.ac.ed.adminremove.plist Disabled -bool false
 launchctl load -w /Library/LaunchDaemons/uk.ac.ed.adminremove.plist
 
 # build log files in /var/admin-logs
-mkdir /var/admin-logs
+[ ! -d "${LOGS}" ] && mkdir "${LOGS}"
 TIME=`date "+Date:%m-%d-%Y TIME:%H:%M:%S"`
-echo $TIME " by " $USERNAME >> /var/admin-logs/15minAdmin
+echo $TIME " by " $USERNAME >> "${LOGS}"/15minAdmin
 
 # note the user
-echo $USERNAME >> /var/admin-logs/userToRemove
+echo $USERNAME >> "${LOGS}"/userToRemove
 
 # give current logged user admin rights
 /usr/sbin/dseditgroup -o edit -a $USERNAME -t user admin
