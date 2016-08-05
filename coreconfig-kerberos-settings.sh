46b063fcd84e581826c21d9184b8dfeeacb94fb7
Geoff Lee <g.lee@ed.ac.uk>
2016-08-05 11:25:35 +0100

    Modify other PAM config to renew ticket on screen unlock

--- coreconfig-kerberos-settings.sh
+++ coreconfig-kerberos-settings.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Script to set up a default Kerberos config file and modify
+# the PAM configiration so a TGT is requested on login and
+# renewed on wake from sleep or screen unlock.
+
 KRB5FILE='/etc/krb5.conf'
-PAMFILE='/etc/pam.d/authorization'
+PAM_AUTHZ='/etc/pam.d/authorization'
+PAM_LOGIN='/etc/pam.d/login'
+PAM_SCREENSAVER='/etc/pam.d/screensaver'
 
 # Make sure that we have a krb5.conf file that looks as we expect
 temp_krb5=`mktemp`
@@ -42,15 +48,18 @@ else
 fi
 
 
-if [ -f ${PAMFILE} ]
-then
-	echo 'Ensuring ${PAMFILE} is up to date...'
-	if egrep '^auth       optional       pam_krb5.so use_first_pass use_kcminit$' ${PAMFILE}
+for file in ${PAM_AUTHZ} ${PAM_LOGIN} ${PAM_SCREENSAVER}
+do
+	if [ -f "${file}" ]
 	then
-		sed -i.$(date "+%Y-%m-%d-%H:%M:%S") \
-				's/^auth       optional       pam_krb5.so use_first_pass use_kcminit$/auth       optional       pam_krb5.so use_first_pass use_kcminit default_principal/'\
-				${PAMFILE}
+		echo 'Ensuring ${file} is up to date...'
+		if egrep '^auth       optional       pam_krb5\.so.*use_kcminit$' "${file}"
+		then
+			sed -E -i.$(date "+%Y-%m-%d-%H:%M:%S") \
+					's/^auth       optional       pam_krb5\.so(.*)use_kcminit$/auth       optional       pam_krb5.so\1use_kcminit default_principal/'\
+					"${file}"
+		fi
+	else
+		echo "Couldn't find ${file}. Something is very wrong!"
 	fi
-else
-	echo "Couldn't find ${PAMFILE}. Something is very wrong!"
-fi
+done
