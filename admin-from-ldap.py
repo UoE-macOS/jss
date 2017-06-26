4efaaaa5d3aaa4458f3ce3b566eded8c091b882b
Geoff Lee <g.lee@ed.ac.uk>
2017-06-26 16:53:48 +0100

    Alterations to make this play nice with the JSS

--- admin-from-ldap.py
+++ admin-from-ldap.py
@@ -13,26 +13,42 @@ import ldap
 import ldap.sasl
 import sys
 import re
-
-SERVER = sys.argv[4] 
-SEARCH_BASE = sys.argv[5] 
-SCOPE = sys.argv[6]
+import os
 
 def main(args):
+    # These should be provided by the JSS.
+    LDAP_SERVER = args[4]
+    LDAP_BASE = args[5]
+
     user = current_user()
+    
+    # Check whether the current user has a Kerberos Ticket-granting Ticket
     if user_has_tgt(user):
-        if len(args) > 1 and args[1] == '--get-admins':
-            print " ".join(get_group_members(computer_name()))
+        # if we have been called with '--get-admins' just print the users 
+        # who should be admins, then exit. 
+        if len(args) > 6 and args[6] == '--get-admins':
+            print " ".join(get_group_members(computer_name(), LDAP_SERVER, LDAP_BASE))
+            sys.exit(0)
         else:
-            # Call ourselves, as the current user, to utilise our TGT
-            members = subprocess.check_output(['launchctl', 'asuser', 
-                                                str(uid(user)), sys.argv[0], 
-                                                '--get-admins']).strip().split(" ") 
-            for mem in members:
-                if not user_is_member_of('admin', mem):
-                    print "Adding {} to group {}".format(mem, 'admin')
-                    add_user_to_group('admin', mem)
+            # Call this script, as the current console user, to utilise their Kerberos Credentials Cache
+            # The three 'dummy' arguments take the place of the arguments the JSS would 
+            # add if it were calling us.
+            members = subprocess.check_output(['launchctl', 'asuser', str(uid(user)), sys.argv[0],
+                                                'dummy', 'dummy', 'dummy', LDAP_SERVER, LDAP_BASE,
+						'--get-admins']).strip().split(" ") 
            
+            if len(members) > 0: 
+                # Make all members of the LDAP group (if found) local admins 
+                for mem in members:
+                    if user_is_local_user(mem):
+                        if not user_is_member_of('admin', mem):
+                            print "Adding {} to group {}".format(mem, 'admin')
+                            add_user_to_group('admin', mem)
+                    else:
+                        print "Not a local user:", mem
+            
+            # Remove any local admins who are not members of the LDAP group (FIXME: this 
+            # should probably be more nuanced)  
             current_admins = get_current_admins()
             for adm in current_admins:
                 if adm not in members:
@@ -42,9 +58,9 @@ def main(args):
     else:
         print "No Kerberos TGT for", current_user()
 
-def ldap_bind():
+def ldap_bind(server):
     try:
-        con=ldap.initialize(SERVER)
+        con=ldap.initialize(server)
         auth = ldap.sasl.gssapi("")
         con.sasl_interactive_bind_s("", auth)
     except Exception, e:
@@ -65,12 +81,15 @@ def user_has_tgt(user):
     try:
        subprocess.check_call(['launchctl', 'asuser', str(uid(user)), 'klist', '-s'])
        return True
-    except subprocess.calledProcessError:
+    except subprocess.CalledProcessError:
        return False
     
-def get_group_members(group):
-    con = ldap_bind()
-    ldap_result_id = con.search(SEARCH_BASE, SCOPE, 'cn=' + group, ['member'])
+def get_group_members(group, server, base):
+    # Search LDAP 'server' for a group named 'group' anywhere under
+    # search base 'base' and return a list of the CNs of group
+    # members.
+    con = ldap_bind(server)
+    ldap_result_id = con.search(base, ldap.SCOPE_SUBTREE, 'cn=' + group, ['member'])
     result_type, result_data = con.result(ldap_result_id)
     members = []
     for mem in result_data[0][1]['member']:
@@ -88,6 +107,17 @@ def user_is_member_of(group, user):
         return True
     except subprocess.CalledProcessError:
         return False
+
+def user_is_local_user(user):
+    try:
+        FNULL = open(os.devnull, 'w')
+        subprocess.check_call(['dscl', '.', '-read', ('/Users/' + user)], stdout=FNULL, stderr=FNULL)
+        return True
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 56:
+            return False
+        else:
+            raise
 
 def add_user_to_group(group, user):
     subprocess.check_call(['dseditgroup', '-o', 'edit', '-a', user, '-t', 'user', group])
