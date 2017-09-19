06b3ebb1a8ad40ef7c7fc5fa4ada49841ed52a4f
Geoff Lee <g.lee@ed.ac.uk>
2017-09-19 17:52:21 +0100

    latest, working version of the generic app request script

--- generic-app-request.py
+++ generic-app-request.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+##########################################################################
+#
+# A generic mechanism for creating application authorisation requests.
+#
+# NB, on its own this script merely creates .apprequest files in a folder.
+# It must be combined with the approprioate extension attribute and
+# moddleware request processing software to be of any use.
+#
+# IN DEVELOPMENT - USE WITH CAUTION.
+#
+# Date: @@DATE
+# Version: @@VERSION
+# Origin: @@ORIGIN
+# Released by JSS User: @@USER
+#
+##########################################################################
+
+
 import json
 import sys
 import os
@@ -23,10 +41,12 @@ def main(args):
     request['date'] = get_now()
     request['UUN'] = get_user()
     request['UUID'] = gen_uuid()
+    request['policy'] = APP_NAME
     request['message'] = msg  
 
     try:
         write_request(request)
+        run_recon()
         display_confirmation()
     except Exception as ex:
         raise
@@ -47,7 +67,7 @@ You'll receive an email when it has been processed." buttons {"OK"} default butt
     
     
 def run_recon():
-    check_call(['/usr/local/bin/jamf', recon])
+    check_call(['/usr/local/bin/jamf', 'recon'])
     
 
 def write_request(request, dir=REQUESTS_DIR):
@@ -59,7 +79,8 @@ def write_request(request, dir=REQUESTS_DIR):
             raise
         elif ex.args[1] == 'File exists':
             pass
-    # Directory should now exist for us to write into
+
+    # The directory should now exist for us to write into
     # If the file already exists, we have a big problem
     out_filename = dir + '/' + request['UUID'] + '.apprequest'
 
