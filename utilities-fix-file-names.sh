84d29ded776a1ba62bd4742c47b10e33afa4a893
David Savage <d.savage@ed.ac.uk>
2018-02-27 13:25:40 +0000

    Add a check for leading spaces

--- utilities-fix-file-names.sh
+++ utilities-fix-file-names.sh
@@ -7,20 +7,21 @@
 # overlong path names and to correct them to help allow smooth
 # synching in OneDrive.
 #
-# Date: Thu 31 Aug 2017 11:30:33 BST
-# Version: 0.1.2
-# Creator: ganders1
+# Date: Thu 27 Feb 2018 10:00:33 BST
+# Version: 0.1.3
+# Creator: dsavage
 #
 ##################################################################
 
-#clear any previos temp files
+# Clear any previous temp files
 rm /tmp/*.ffn
 
-#get the user
+# Get the user
 uun=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
-#remove local fstemps so they won't clog the server
+# Remove local fstemps so they won't clog the server
 find /Users/$uun -name ".fstemp*" -exec rm -dfR '{}' \;
+
 # Find the OneDrive folder location assuming it has OneDrive in the name...
 onedrive=`find /Users/$uun -type d -name "OneDrive*" -not -path "*/Library*" -not -path "*/Music*" -not -path "*/Pictures*" -not -path "*/Downloads*" -not -path "*.pkg" -not -path ".Trash"`
 
@@ -30,8 +31,8 @@ Check_Long_Names ()
 echo File paths longer than 230 characters may not synchronise to the server. The following are the files that are in that category on your system and have now been temporarily moved to a folder called LongFileNames inside your home folder. If you could please shorten the names of these files and move the shortened versions back into either your Desktop or Documents folder, they will then synchronise to the server as normal: > /tmp/clnOut.ffn
 echo " " >> /tmp/clnOut.ffn
 linecount=`wc -l /tmp/cln.ffn | awk '{print $1}'`
-counter=1
-while ! [ "$counter" -gt "$linecount" ]; do
+counter=$linecount
+while ! [ "$counter" == 0 ]; do
 
 line="`sed -n ${counter}p /tmp/cln.ffn`"
 
@@ -44,7 +45,7 @@ open /Users/$uun/LongFileNames
 echo Number of Chars = $test   Path = $line 
 echo Number of Chars = $test   Path = $line >> /tmp/clnOut.ffn
 fi
-let "counter = $counter +1"
+let "counter = $counter -1"
 done
 }
 
@@ -53,87 +54,103 @@ Check_Trailing_Chars ()
 
 cat /tmp/cln.ffn | grep -v ".pkg" | grep -v ".app" > /tmp/fixtrail.ffn
 linecount=`wc -l /tmp/fixtrail.ffn | awk '{print $1}'`
-counter=1
-while ! [ "$counter" -gt "$linecount" ]; do
+counter=$linecount
+while ! [ "$counter" == 0 ]; do
 
 line="`sed -n ${counter}p /tmp/fixtrail.ffn`"
 lastChar="`sed -n ${counter}p /tmp/fixtrail.ffn | grep -Eo '.$'`"
 
 if [ "$lastChar" == " " ] || [ "$lastChar" == "." ]
 then
+name=`basename "$line"` # get the filename we need to change
+path=`dirname "$line"` # dirname to get the path
+fixedname=$(echo "$name" | tr '.' '-' | awk '{sub(/[ \t]+$/, "")};1') # remove/replace the trailing whitespace or period
 
-target=$(echo "$line" | tr '.' '-' | awk '{sub(/[ \t]+$/, "")};1')
-echo "$target" >> /tmp/allfixed.ffn
-mv -f "$line" "$target"
+mv -f "$line" "$path/$fixedname" # rename the file or folder
 
 fi
 
-let "counter = $counter +1"
+let "counter = $counter -1"
+done
+}
+
+
+Check_Leading_Spaces ()
+{
+
+cat /tmp/cln.ffn | grep -v ".pkg" | grep -v ".app" | grep "/[[:space:]]" > /tmp/fixlead.ffn
+linecount=`wc -l /tmp/fixlead.ffn | awk '{print $1}'`
+counter=$linecount
+while ! [ "$counter" == 0 ]; do
+
+line="`sed -n ${counter}p /tmp/fixlead.ffn`"
+name=`basename "$line"` # get the filename we need to change
+path=`dirname "$line"` # dirname to get the path
+fixedname=`echo $name | sed -e 's/^[ \t]*//'` # sed out the leading whitespace
+echo "This is the original - $line"
+echo "This is the fix - $path/$fixedname"
+mv -f "$line" "$path/$fixedname" # rename the file or folder
+
+let "counter = $counter -1"
 done
 }
 
 
 Fix_Names ()
 {
-#count the number of lines in the fixdname.ffn or fixfname.ffn file
+# Count the number of lines in the fixdname.ffn or fixfname.ffn file
 linecount=`wc -l /tmp/${1}.ffn | awk '{print $1}'`
-counter=1
-while ! [ "$counter" -gt "$linecount" ]; do
+counter=$linecount
+while ! [ "$counter" == 0 ]; do
 
 line="`sed -n ${counter}p /tmp/${1}.ffn`"
-echo line= $line
+name=`basename "$line"` # get the filename we need to change
+path=`dirname "$line"` # dirname to get the path
+fixedname=$(echo "$name" | tr ':' '-' | tr '\\\' '-' | tr '?' '-' | tr '*' '-' | tr '"' '-' | tr '<' '-' | tr '>' '-' | tr '%' '-' | tr '|' '-' ) # sed out the leading whitespace
+echo "$fixedname" >> /tmp/allfixed.ffn
 
-target=$(echo "$line" | tr ':' '-' | tr '\\\' '-' | tr '?' '-' | tr '*' '-' | tr '"' '-' | tr '<' '-' | tr '>' '-' | tr '%' '-' | tr '|' '-' )
-echo "$target"
-echo "$target" >> /tmp/allfixed.ffn
+mv -f "$line" "$path/$fixedname" # rename the file or folder
 
-mv -f "$line" "$target"
-
-let "counter = $counter +1"
+let "counter = $counter -1"
 done
 }
 
+find /Users/$uun/Documents -name '*[\\/:*?"<>%|]*' -print >> /tmp/acln.ffn
+find /Users/$uun/Desktop -name '*[\\/:*?"<>%|]*' -print >> /tmp/acln.ffn
+find "${onedrive}" -name '*[\\/:*?"<>%|]*' -print >> /tmp/acln.ffn
 
-#do the finds for the Docs/Desk directories only
-echo get Documents folders
-find /Users/$uun/Documents -type d -name '*[\\/:*?"<>%|]*' -print >> /tmp/fixdname.ffn
+# First pass at fixing the file names
+Fix_Names acln
+sleep 1
+echo "++++++++++++++++++ fixed illegal chars"
+#rm -f /tmp/cln.ffn
+open /tmp/acln.ffn
 
-echo get Desktop folders
-find /Users/$uun/Desktop -type d -name '*[\\/:*?"<>%|]*' -print  >> /tmp/fixdname.ffn
+find /Users/$uun/Documents -name "*" >> /tmp/cln.ffn
+find /Users/$uun/Desktop -name "*" >> /tmp/cln.ffn
+find "${onedrive}" -name "*" >> /tmp/cln.ffn
 
-#need to fix directories before we fix files
-Fix_Names fixdname
+Check_Trailing_Chars
+echo "++++++++++++++++++ fixed trailing spaces and periods"
+sleep 1
+rm -f /tmp/cln.ffn
 
-#do the finds for the Docs/Desk files only
-echo get Documents files
-find /Users/$uun/Documents -type f -name '*[\\/:*?"<>%|]*' -print >> /tmp/fixfname.ffn
+find /Users/$uun/Documents -name "*" >> /tmp/cln.ffn
+find /Users/$uun/Desktop -name "*" >> /tmp/cln.ffn
+find "${onedrive}" -name "*" >> /tmp/cln.ffn
 
-echo get Desktop files
-find /Users/$uun/Desktop -type f -name '*[\\/:*?"<>%|]*' -print >> /tmp/fixfname.ffn
-
-#fix the files
-Fix_Names fixfname
-
-
-# OneDrive needs to be separate incase it exists in Docs or Desk, find dirs
-find "${onedrive}" -type d -name '*[\\/:*?"<>%|]*' -print  >> /tmp/fix1dname.ffn
-
-#need to fix OneDrive directories before we fix files
-Fix_Names fix1dname
-
-# OneDrive needs to be separate incase it exists in Docs or Desk, find files
-find "${onedrive}" -type f -name '*[\\/:*?"<>%|]*' -print >> /tmp/fix1fname.ffn
-
-#fix the OneDrive files
-Fix_Names fix1fname
-
+Check_Leading_Spaces
+echo "++++++++++++++++++ fixed leading spaces"
+sleep 1
+rm -f /tmp/cln.ffn
 
 find /Users/$uun/Documents -name "*" >> /tmp/cln.ffn
 find /Users/$uun/Desktop -name "*" >> /tmp/cln.ffn
 find "${onedrive}" -name "*" >> /tmp/cln.ffn
 
 Check_Long_Names
-Check_Trailing_Chars
+sleep 1
+rm -f /tmp/cln.ffn
 
 testlong=`grep "$uun" /tmp/clnOut.ffn`
 
@@ -147,9 +164,9 @@ Send_Message ()
 {
 
 cat <<EOF > /tmp/message.txt
-It is necessary for folder or file names containing any illegal characters to be renamed. These characters are the following: \ / : * ? " < > % | they also include trailing . and trailing spaces.
+It is necessary for folder or file names containing any illegal characters to be renamed. These characters are the following: \ / : * ? " < > % | they also include leading spaces, trailing . and trailing spaces.
 
-This message is to advise you of any such files or folders that have been affected so that you are aware that of these characters in their names will now have been replaced by a hyphen. 
+This message is to advise you of any such files or folders that have been affected so that you are aware that of these characters in their names will now have been replaced by a hyphen or removed. 
 
 Here are the files and folders that have been renamed in your case:
 
@@ -215,6 +232,6 @@ Send_Message
 fi
 
 # cleanup
-rm /tmp/*.ffn
+#rm /tmp/*.ffn
 
 exit 0;
