32ddec1ef99ffe4295b9566e844f37c4526936cc
David Savage <d.savage@ed.ac.uk>
2018-12-20 14:19:06 +0000

    typo

--- coreconfig-filevault-login-check.sh
+++ coreconfig-filevault-login-check.sh
@@ -6,7 +6,7 @@
 # if filevault is currently disabled and the user logging
 # in is a valid user in our directory service.
 #
-# Date: Thu 26 Apr 2018 15:42:12 BST
+# Date: Thu 20 Dec 2018 14:17:54 GMT
 # Version: 0.1.7
 # Origin: https://github.com/UoE-macOS/jss.git
 # Released by JSS User: dsavage
@@ -32,16 +32,22 @@ filevault_is_enabled() {
 
 # in-built jamf variable $3, doesn't seem to be returning a valid username, even if a uun account is logged on.
 user_name=`ls -l /dev/console | awk '{print $3}'`
+# Volume format
+VOL=`diskutil info / | grep Personality | awk -F':' ' { print $NF } ' | sed -e 's/^[[:space:]]*//'`
 
 if ! filevault_is_enabled
 then
     # Reset the filevault status incase the defered enablement is broken.
     fdesetup disable
     rm -f /Library/Preferences/com.apple.fdesetup.plist
+    # Check if apfs
+    if [ "$VOL" == "APFS" ]; then
+    	diskutil apfs updatePreboot /
+    fi
     # This causes the 'UoE - FileVault - Initialise' policy to
     # set things up such that FileVault will be enabled for the
     # current user on next logout
-    /usr/local/bin/jamf policy -event 'filevault-init'
+    /usr/local/bin/jamf policy -event filevault-init
     
     # Now force the user to log out to complete the enablement process
 	HELPER=`"${jamfHelper}" -windowType utility -icon "$icon" -heading "$heading" -description "$description" -button1 "Log Out Now"`
