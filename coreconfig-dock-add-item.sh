3fa28f445d1f9f2411e5b5062eeca1c3aaf95d1d
David Savage <d.savage@ed.ac.uk>
2019-03-19 16:14:05 +0000

    Update to let script generate url
    can generate a url file with an icon to a link in the dock as well as adding an app or folder.

--- coreconfig-dock-add-item.sh
+++ coreconfig-dock-add-item.sh
@@ -8,81 +8,116 @@
 # The script takes 3 arguments; the path to the application
 # to be added and two related to the dock item position.
 #
-# Last Changed: "Wed 02 Aug 2017 15:30:04"
-# Version: 0.1.3
+# Last Changed: "Tue 19 Mar 2019 16:12:35 GMT"
+# Version: 0.1.4
 # Origin: https://github.com/UoE-macOS/jss.git
 # Released by JSS User: dsavage
 #
 ##################################################################
 
-# we need to wait for the dock to actually start 
-until [[ $(pgrep Dock) ]]; do
-    wait
-done
 
-ACTIVE_USER=`ls -l /dev/console | awk '{print $3}'`
+ACTIVE_USER=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
 echo "Active user is $ACTIVE_USER"
+
+
+if [ -z "${ACTIVE_USER}" ] || [ "${ACTIVE_USER}" == "root" ] || [ "${ACTIVE_USER}" == "_mbsetupuser" ] || [ "${ACTIVE_USER}" == "" ]; then
+	exit 0;
+fi
+
+# we need to wait for the dock to actually start if a user is present
+#until [[ $(pgrep Dock) ]]; do
+#    wait
+#done
 
 if [ -f /Users/$ACTIVE_USER/.NoDock ]; then
 exit 0;
 fi
 
-DOCK_PREF="/Users/$ACTIVE_USER/Library/Preferences/com.apple.dock.plist"
+sleep 3
+echo "Pref file is: ${DOCK_PREF}"
+if [ -f "${DOCK_PREF}" ]; then
+	echo "Dock preference exists."
+else
+	su $ACTIVE_USER -c /System/Library/CoreServices/Dock.app/Contents/MacOS/Dock
+    echo "Force launching the Dock."
+fi
 
+DOCK_PREF="/Users/$ACTIVE_USER/Library/Preferences/com.apple.dock.plist"
 echo "The Dock Pref plist is $DOCK_PREF"
 
 # File to store if dock was changed.
 DOCK_CHANGE="/tmp/dock-change.txt"
-
 echo "The Dock Change text file is at $DOCK_CHANGE"
 
 # Set the path for the dock command line tool.
 DOCK_UTIL="/usr/local/bin/dockutil"
-
 echo "The Dock Utility is at $DOCK_UTIL"
 
 # Path to the application, normally /Applications/****.app
-APPLICATION_PATH="$4"
-
-echo "The Application Path is $APPLICATION_PATH"
+ITEM_PATH="$4"
+echo "The Application Path is $ITEM_PATH"
 
 POSITION="$5"
-
 echo "The Application position is $POSITION"
 
 POSITION_KEY="$6"
-
 echo "The Application position key is $POSITION_KEY"
 
-# Set the application name.
-APP_NAME=`basename "$APPLICATION_PATH" | awk -F "." '{print $1}'`
+GENERATE_URL="$7"
+echo "The url is $GENERATE_URL"
 
-echo "The App Name is $APP_NAME"
+ICON="$8"
+echo "The icon is $ICON"
+
+# Set the name.
+ITEM_NAME=`basename "$ITEM_PATH" | awk -F "." '{print $1}'`
+
+echo "The item to be added is: $ITEM_NAME"
+
+# Set the type .app / .url normally
+ITEM_TYPE=`basename "$ITEM_PATH" | awk -F "." '{print $2}'`
+
+echo "The item type to be added is: $ITEM_TYPE"
+
+if ! [ -z "${GENERATE_URL}" ] && [ "${ITEM_TYPE}" == "url" ]; then
+	echo '[InternetShortcut]' > "${ITEM_PATH}"
+	echo -n 'URL=' >> "${ITEM_PATH}"
+	echo "${GENERATE_URL}" >> "${ITEM_PATH}"
+	PY_ICON=`echo "\"${ICON}\""`
+	PY_PATH=`echo "\"${ITEM_PATH}\""`
+	python -c "import Cocoa; Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(Cocoa.NSImage.alloc().initWithContentsOfFile_(${PY_ICON}), ${PY_PATH}, 0)"
+fi
 
 # Check if item is already in dock.
-CHECK_ITEM=$(defaults read $DOCK_PREF | grep "file-label" | awk -F "=" '{print $2}' | sed -e 's/^[[:space:]]*//' | grep "${APP_NAME}")
+CHECK_ITEM=$(defaults read $DOCK_PREF | grep "file-label" | awk -F "=" '{print $2}' | sed -e 's/^[[:space:]]*//' | grep "${ITEM_NAME}")
 # Fix an issue with how defaults returns labels which contain a space.
 if [[ "$CHECK_ITEM" =~ '"' ]]; then
 	CHECK_ITEM=$(echo "$CHECK_ITEM" | awk -F '"' '{print $2}')
 fi
 # Make sure that we only get a single response
-CHECK_ITEM=$(echo "$CHECK_ITEM" | grep -E "^${APP_NAME}")
+CHECK_ITEM=$(echo "$CHECK_ITEM" | grep -E "^${ITEM_NAME}")
 
 echo "Check Item is $CHECK_ITEM"
 
-if [ "$CHECK_ITEM" == "$APP_NAME;" ] || [ "$CHECK_ITEM" == "$APP_NAME" ]; then
+if [ "$CHECK_ITEM" == "$ITEM_NAME;" ] || [ "$CHECK_ITEM" == "$ITEM_NAME" ]; then
 	echo no action taken >> $DOCK_CHANGE
 else
-	if [ -e "$APPLICATION_PATH" ]; then
-	    if [ -z "$POSITION" ] || [ "$POSITION" == '' ]; then
-	        $DOCK_UTIL --add "$APPLICATION_PATH" --no-restart $DOCK_PREF
-			echo dock item added >> $DOCK_CHANGE 
-	    else
-		    $DOCK_UTIL --add "$APPLICATION_PATH" "$POSITION" "$POSITION_KEY" --no-restart $DOCK_PREF 
-		    echo dock item added >> $DOCK_CHANGE
+	if [ -e "$ITEM_PATH" ]; then
+		if [ "${ITEM_TYPE}" == "app" ]; then
+	    	if [ -z "$POSITION" ] || [ "$POSITION" == '' ]; then
+	        	$DOCK_UTIL --add "$ITEM_PATH" --no-restart $DOCK_PREF
+				echo dock item added >> $DOCK_CHANGE 
+	    	else
+		    	$DOCK_UTIL --add "$ITEM_PATH" "$POSITION" "$POSITION_KEY" --no-restart $DOCK_PREF 
+		    	echo dock item added >> $DOCK_CHANGE
+			fi
+		else
+			$DOCK_UTIL --add "$ITEM_PATH" --section others --no-restart $DOCK_PREF
+          	echo dock item added >> $DOCK_CHANGE
 		fi
 	fi
 fi
 
 exit 0;
+
