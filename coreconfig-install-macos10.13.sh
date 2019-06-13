b2331193860cbb2e4c83c0de33f25fd15e087ed2
Gavin Anderson <gav.anderson@ed.ac.uk>
2019-06-13 16:43:45 +0100

    Remove OS installer copying

--- coreconfig-install-macos10.13.sh
+++ coreconfig-install-macos10.13.sh
@@ -10,7 +10,6 @@
 #
 ##################################################################
 
-date > /Users/Shared/installTST.log
 
 # # # # # # 
 # SYSTEM CHECKS
@@ -24,17 +23,17 @@ then
 exit 0
 fi
 
-##Check if free space > 15GB
+# Check if free space > 15GB
 freeSpace=$( /usr/sbin/diskutil info / | grep "Free Space" | awk '{print $4}' )
 if [[ ${freeSpace%.*} -ge 15 ]]; then
     spaceStatus="OK"
-    /bin/echo "Disk Check: OK - ${freeSpace%.*} Free Space Detected" >> /Users/Shared/installTST.log
+    /bin/echo "Disk Check: OK - ${freeSpace%.*} Free Space Detected"
 else
     spaceStatus="ERROR"
-    /bin/echo "Disk Check: ERROR - ${freeSpace%.*} Free Space Detected" >> /Users/Shared/installTST.log
+    /bin/echo "Disk Check: ERROR - ${freeSpace%.*} Free Space Detected"
 fi
 
-username=`who | grep console | awk '{print $1}'`
+username=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
 if [ -z $username ]; then
 	NoUser=True
 else
@@ -56,64 +55,59 @@ This process will take approximately 10-15 minutes.
 Once completed your computer will reboot and begin the install."
 
 ##Icon to be used for jamfHelper
-
-icon=/Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/InstallAssistant.icns
+if [ -f /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/InstallAssistant.icns ]; then
+	icon=/Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/InstallAssistant.icns
+else
+	icon=/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns 
+fi
 
 ##Launch jamfHelper
-
 if [ $NoUser == False ]; then
 /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType fs -title "" -icon "$icon" -heading "$heading" -description "$description" &
 jamfHelperPID=$(echo $!)
 fi
+
 ##Start macOS Upgrade
 macOS_app_vers=`defaults read "/Applications/Install macOS High Sierra.app/Contents/Info" CFBundleShortVersionString | awk -F "." '{print $1$2}'`
-macOS_loc_vers=`defaults read "/Library/MacSD/Install macOS High Sierra.app/Contents/Info" CFBundleShortVersionString | awk -F "." '{print $1$2}'`
 echo $macOS_app_vers
-
 if [ -z $macOS_app_vers ]; then
 	macOS_app_vers=126
 fi
 
 if [ $macOS_app_vers -ge 134 ]; then
-	echo "first test macOS 13.4 or newer present"
-	# Check if we already have a copy of the installer
-	if [ $macOS_loc_vers -ge 134 ]; then
-    
-    	echo "Second test local MacSD macOS 13.4 or newer present"
-        # Copy the installer to our folder so we can retain it for future use
-		ditto "/Applications/Install macOS High Sierra.app" "/Library/MacSD/Install macOS High Sierra.app"
-        # delete the login banner as we are updating macOS
-		rm -fR /Library/Security/PolicyBanner.rtfd
-        # Create the upgrade flag to ensure a recon after the upgrade.
-		touch /Library/MacSD/SUDONE
 
-        if [ $NoUser == True ]; then
-			/Library/MacSD/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Library/MacSD/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense 
-		else
-            /Library/MacSD/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Library/MacSD/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense --pidtosignal $jamfHelperPID &
-			osascript -e 'tell application "Self Service" to quit'
-        fi
+    # delete the login banner as we are updating macOS
+	rm -fR /Library/Security/PolicyBanner.rtfd
+    
+    # Create the upgrade flag to ensure a recon after the upgrade.
+	touch /Library/MacSD/SUDONE
+    if [ $NoUser == True ]; then
+		/Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Applications/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense 
 	else
-		# Do a delete incase an older version is there
-		rm -fR "/Library/MacSD/Install macOS High Sierra.app"
-        # Add the installer
-        /usr/local/bin/jamf policy -event OS-Installer
-		# Copy the installer to our folder so we can retain it for future use
-		ditto "/Applications/Install macOS High Sierra.app" "/Library/MacSD/Install macOS High Sierra.app"
-        # delete the login banner as we are updating macOS
-		rm -fR /Library/Security/PolicyBanner.rtfd        
-        # Create the upgrade flag to ensure a recon after the upgrade.
-		touch /Library/MacSD/SUDONE
-        
-        if [ $NoUser == True ]; then
-			/Library/MacSD/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Library/MacSD/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense 
-		else
-			/Library/MacSD/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Library/MacSD/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense --pidtosignal $jamfHelperPID &
-			osascript -e 'tell application "Self Service" to quit'
-        fi
+        /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Applications/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense --pidtosignal $jamfHelperPID &
+		osascript -e 'tell application "Self Service" to quit'
+    fi
+    
+else
+
+	# Do a delete incase an older version is there
+	rm -fR "/Applications/Install macOS High Sierra.app"
+    
+    # Add the installer
+    /usr/local/bin/jamf policy -event OS-Installer
+    
+    # Delete the login banner as we are updating macOS
+	rm -fR /Library/Security/PolicyBanner.rtfd       
+    
+    # Create the upgrade flag to ensure a recon after the upgrade.
+	touch /Library/MacSD/SUDONE   
+    
+    if [ $NoUser == True ]; then
+		/Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Applications/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense 
+	else
+		/Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/startosinstall --applicationpath /Applications/Install\ macOS\ High\ Sierra.app --nointeraction --agreetolicense --pidtosignal $jamfHelperPID &
+		osascript -e 'tell application "Self Service" to quit'
     fi
 fi
-
-
 
 exit 0;
