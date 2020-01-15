27f873973f81f604479c51f349050c5a10eec080
colemanross <r.coleman@ed.ac.uk>
2020-01-15 15:16:42 +0000

    Update to NoLoAD

--- NoLoAD-postinstall.sh
+++ NoLoAD-postinstall.sh
@@ -3,54 +3,103 @@
 # Preference key reference
 # https://gitlab.com/orchardandgrove-oss/NoMADLogin-AD/wikis/Configuration/preferences
 
+# Function for obtaining timestamp
+timestamp() {
+    while read -r line
+    do
+        TIMESTAMP=`date`
+        echo "[$TIMESTAMP] $line"
+    done
+}
+
+# Log file location
+LOGFILE="/Library/Logs/NoLoAD.log"
+
 # Set domain
-domain="ed.ac.uk"
+DOMAIN="ed.ac.uk"
 
 # Set background image transparancy
-background_image_alpha=9
+BACKGROUND_IMAGE_ALPHA=1
 
 # Set logo location
-logo="/usr/local/jamf/UoELogo.png"
+LOGO="/usr/local/jamf/UoELogo.png"
 
 # Place holder for username text field
-placeholder="University username"
+PLACEHOLDER="Your University Login"
 
-# Depedning on OS Version, set the background image
-os_version=$( sw_vers -productVersion | awk -F '.' '{print $2}' )
-case $os_version in
+# Set plist location
+PLIST="/Library/Preferences/menu.nomad.login.ad.plist"
+
+ADMIN="YES"
+
+echo "*** Installing NoMAD Login AD and setting preferences ****" | timestamp 2>&1 | tee -a $LOGFILE
+
+# Depending on OS Version, set the background image
+OS_VERSION=$( sw_vers -productVersion | awk -F '.' '{print $2}' )
+echo "OS Version is $OS_VERSION" | timestamp 2>&1 | tee -a $LOGFILE
+case $OS_VERSION in
 	12)
-	background_image="/Library/Desktop Pictures/Sierra.jpg"
+	BACKGROUND_IMAGE="/Library/Desktop Pictures/Sierra.jpg"
 	;;
 	13)
-	background_image="/Library/Desktop Pictures/High Sierra.jpg"
+	BACKGROUND_IMAGE="/Library/Desktop Pictures/High Sierra.jpg"
 	;;
 	14)
-	background_image="/Library/Desktop Pictures/Mojave Night.jpg"
+	BACKGROUND_IMAGE="/Library/Desktop Pictures/Mojave Night.jpg"
+	;;
+	15)
+	# Apple have changed their default location for system wallpapers to /System in 10.15, so probably protected by SIP.
+	# They've also converted wallpapers to .heic format which NoLoAD seems to have a problem with, so pointing to a Catalina converted .jpg file not in /System.
+	# Converted .jpg file should be in place as packaged as part of NoLoAD
+	BACKGROUND_IMAGE="/usr/local/jamf/Catalina Night.jpg"
 	;;
 	*)
-	background_image="/usr/local/jamf/Black.png"
+	BACKGROUND_IMAGE="/usr/local/jamf/Black.png"
 	;;
 esac
 
+# Make every account that logs in an admin
+echo "Setting admin user creation..." | timestamp 2>&1 | tee -a $LOGFILE
+defaults write $PLIST CreateAdminUser -bool "$ADMIN"
+
 # Set default AD domain
-defaults write /Library/Preferences/menu.nomad.login.ad.plist ADDomain "$domain"
+echo "Setting domain to ed.ac.uk..." | timestamp 2>&1 | tee -a $LOGFILE
+defaults write $PLIST ADDomain "$DOMAIN"
 
 # Set background image
-defaults write /Library/Preferences/menu.nomad.login.ad.plist BackgroundImage "$background_image"
+echo "Setting background image depending on OS version..." | timestamp 2>&1 | tee -a $LOGFILE
+defaults write $PLIST BackgroundImage "$BACKGROUND_IMAGE"
 
 # Set background image transparency
-defaults write /Library/Preferences/menu.nomad.login.ad.plist BackgroundImageAlpha -int "$background_image_alpha"
+echo "Setting background image transparency..." | timestamp 2>&1 | tee -a $LOGFILE
+defaults write $PLIST BackgroundImageAlpha -int "$BACKGROUND_IMAGE_ALPHA"
 
 # Set login window logo
-defaults write /Library/Preferences/menu.nomad.login.ad.plist LoginLogo "$logo"
+echo "Setting university logo location..." | timestamp 2>&1 | tee -a $LOGFILE
+defaults write $PLIST LoginLogo "$LOGO"
 
 # A String to show as the placeholder in the Username textfield.
-defaults write /Library/Preferences/menu.nomad.login.ad.plist UsernameFieldPlaceholder "$placeholder"
+echo "Setting placeholder text..." | timestamp 2>&1 | tee -a $LOGFILE
+defaults write $PLIST UsernameFieldPlaceholder "$PLACEHOLDER"
+
+# Wait for setup to complete before loading the NoMAD login window
+CURRENT_USER=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+until [ "$CURRENT_USER" != "_mbsetupuser" ]
+do
+    CURRENT_USER=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+    echo "Current user is $CURRENT_USER" | timestamp 2>&1 | tee -a $LOGFILE
+    echo "Waiting for setup to complete..." | timestamp 2>&1 | tee -a $LOGFILE
+    sleep 5    
+done
 
 # Set security authorization database mechanisms with authchanger
+echo "Setting security authorization database mechanisms..." | timestamp 2>&1 | tee -a $LOGFILE
 /usr/local/bin/authchanger -reset -AD
 
 # Kill loginwindow process to force NoMAD Login to launch
+echo "Killing the login window..." | timestamp 2>&1 | tee -a $LOGFILE
 /usr/bin/killall -HUP loginwindow
+
+echo "NoMAD Login AD successfully installed!" | timestamp 2>&1 | tee -a $LOGFILE
 
 exit 0
